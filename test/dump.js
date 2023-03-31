6306026b97408a83eb79a10f89d848223ad8b5e3
Sanjeeban2411 <sanju.sanjeeban.sp@gmail.com>
2023-03-31 19:42:21 +0530

    trails

--- test/dump.js
+++ test/dump.js
@@ -8,45 +8,47 @@ const app = express();
 //   const url = await ngrok.connect();
 // })();
 
-app.get("/generatekeys", async function paillierTest(req, res) {
-  const { publicKey, privateKey } = await paillier.generateRandomKeys(64);
-  const pub = publicKey;
-  const pvt = {
-    lambda: privateKey.lambda,
-    mu: privateKey.mu,
-    _p: privateKey._p,
-    _q: privateKey._q,
-  };
-  const pvtJson = JSON.stringify(pvt, (key, value) => {
-    return typeof value === "bigint" ? value.toString() : value;
-  });
-  const outputPvt = JSON.parse(pvtJson);
+// app.get("/generatekeys", async function paillierTest(req, res) {
+//   const { publicKey, privateKey } = await paillier.generateRandomKeys(64);
+//   const pub = publicKey;
+//   const pvt = {
+//     lambda: privateKey.lambda,
+//     mu: privateKey.mu,
+//     _p: privateKey._p,
+//     _q: privateKey._q,
+//   };
+//   const pvtJson = JSON.stringify(pvt, (key, value) => {
+//     return typeof value === "bigint" ? value.toString() : value;
+//   });
+//   const outputPvt = JSON.parse(pvtJson);
 
-  const pubJson = JSON.stringify(pub, (key, value) => {
-    return typeof value === "bigint" ? value.toString() : value;
-  });
-  const outputPub = JSON.parse(pubJson);
+//   const pubJson = JSON.stringify(pub, (key, value) => {
+//     return typeof value === "bigint" ? value.toString() : value;
+//   });
+//   const outputPub = JSON.parse(pubJson);
 
-  res.send({ outputPub, outputPvt });
-});
+//   res.send({ outputPub, outputPvt });
+// });
 
-app.listen(8080, () => {
-  console.log("listening on 8080");
-});
+// app.listen(8080, () => {
+//   console.log("listening on 8080");
+// });
 
-// async function paillierTest(req, res) {
-//   const { publicKey, privateKey } = await paillier.generateRandomKeys(32)
-//   console.log("pub",publicKey, "\npvt", privateKey)
 
-//   //(A-B)^2
-//   let a = 40;
-//   let b = 100;
 
-//   // a = a*(-1)
-//   const a1 = publicKey.encrypt(a);
-//   const b1 = publicKey.encrypt(b);
-//   console.log("A1", a1)
-//   console.log("B1", b1)
+async function paillierTest(req, res) {
+  const { publicKey, privateKey } = await paillier.generateRandomKeys(32)
+  console.log("pub",publicKey, "\npvt", privateKey)
+
+  //(A-B)^2
+  let a = 40;
+  let b = 100;
+
+  // a = a*(-1)
+  const a1 = publicKey.encrypt(a);
+  const b1 = publicKey.encrypt(b);
+  console.log("A1", a1)
+  console.log("B1", b1)
 
 //   const b2  =publicKey.multiply(b1,-1)
 
@@ -63,5 +65,5 @@ app.listen(8080, () => {
 //   const x_enc = publicKey.encrypt(x)
 //   console.log("test_enc", x_enc)
 //   console.log("text_dec", privateKey.decrypt(x_enc))
-// }
-// paillierTest();
+}
+paillierTest();
